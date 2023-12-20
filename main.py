efc26a612b03898ef8437cecc3ba0b39766a77a4
katya <kgracheva03@gmail.com>
2023-12-20 16:50:49 +0300

    loading photos function

--- main.py
+++ main.py
@@ -8,3 +8,53 @@ import cv2
 import tensorflow as tf
 from tqdm import tqdm
 
+class_names = ['astilbe', 'bellflower', 'black_eyed_susan', 'calendula', 'california_poppy', 'carnation', 'common_daisy', 'coreopsis', 'dandelion', 'iris', 'rose', 'sunflower', 'tulip', 'water_lily']
+class_names_label = {class_name:i for i, class_name in enumerate(class_names)}
+
+print(class_names_label)
+
+nb_classes = len(class_names)
+
+IMAGE_SIZE = (256, 256)
+
+
+
+
+def load_data():
+    datasets = ['./images/train', './images/val']
+    output = []
+    
+    for dataset in datasets:
+        print(dataset)
+        images = []
+        labels = []
+        
+        print("Loading {}".format(dataset))
+        
+        # Iterate through each folder corresponding to a category
+        for folder in os.listdir(dataset):
+            label = class_names_label[folder]
+            
+            # Iterate through each image in our folder
+            for file in tqdm(os.listdir(os.path.join(dataset, folder))):
+                
+                # Get the path name of the image
+                img_path = os.path.join(os.path.join(dataset, folder), file)
+                
+                # Open and resize the img
+                image = cv2.imread(img_path)
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                image = cv2.resize(image, IMAGE_SIZE) 
+                
+                # Append the image and its corresponding label to the output
+                images.append(image)
+                labels.append(label)
+                
+        images = np.array(images, dtype = 'float32')
+        labels = np.array(labels, dtype = 'int32')   
+        
+        output.append((images, labels))
+
+    return output
+
+print(load_data())
