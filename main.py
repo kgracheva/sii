6a2c85a3f8306d6486af7e7eb8a0792d11032533
katya <kgracheva03@gmail.com>
2023-12-20 17:18:53 +0300

    before main part of task

--- main.py
+++ main.py
@@ -7,18 +7,7 @@ import matplotlib.pyplot as plt
 import cv2
 import tensorflow as tf
 from tqdm import tqdm
-
-class_names = ['astilbe', 'bellflower', 'black_eyed_susan', 'calendula', 'california_poppy', 'carnation', 'common_daisy', 'coreopsis', 'dandelion', 'iris', 'rose', 'sunflower', 'tulip', 'water_lily']
-class_names_label = {class_name:i for i, class_name in enumerate(class_names)}
-
-print(class_names_label)
-
-nb_classes = len(class_names)
-
-IMAGE_SIZE = (256, 256)
-
-
-
+import pandas as pd
 
 def load_data():
     datasets = ['./images/train', './images/val']
@@ -55,6 +44,53 @@ def load_data():
         
         output.append((images, labels))
 
-    return output
+    return output #массив чисел для каждой фотки (видимо представление фотки в битах или байтах?)
 
-print(load_data())
+def display_random_image(class_names, images, labels):    
+    index = np.random.randint(images.shape[0])
+    plt.figure()
+    plt.imshow(images[index])
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.title('Image #{} : '.format(index) + class_names[labels[index]])
+    plt.show()
+
+class_names = ['astilbe', 'bellflower', 'black_eyed_susan', 'calendula', 'california_poppy']
+# , 'carnation', 'common_daisy', 'coreopsis', 'dandelion', 'iris', 'rose', 'sunflower', 'tulip', 'water_lily'
+class_names_label = {class_name:i for i, class_name in enumerate(class_names)}
+
+print(class_names_label)
+
+nb_classes = len(class_names)
+
+IMAGE_SIZE = (256, 256)
+
+(train_images, train_labels), (test_images, test_labels) = load_data()
+
+#random_state??
+train_images, train_labels = shuffle(train_images, train_labels, random_state=25)
+
+n_train = train_labels.shape[0]
+n_test = test_labels.shape[0]
+
+print ("Number of training examples: {}".format(n_train))
+print ("Number of testing examples: {}".format(n_test))
+print ("Each image is of size: {}".format(IMAGE_SIZE))
+
+
+_, train_counts = np.unique(train_labels, return_counts=True)
+_, test_counts = np.unique(test_labels, return_counts=True)
+pd.DataFrame({'train': train_counts,
+                    'test': test_counts}, 
+             index=class_names
+            ).plot.bar()
+plt.show()
+
+train_images = train_images / 255.0 
+test_images = test_images / 255.0
+
+
+display_random_image(class_names, train_images, train_labels)
+
+
