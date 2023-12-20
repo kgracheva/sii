0a1e1c87300eef207effc799a800b73d9ca6bf45
katya <kgracheva03@gmail.com>
2023-12-20 15:56:59 +0300

    import libraries

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+import os
+from sklearn.metrics import confusion_matrix
+import seaborn as sn; sn.set(font_scale=1.4)
+from sklearn.utils import shuffle
+import matplotlib.pyplot as plt
+import cv2
+import tensorflow as tf
+from tqdm import tqdm
+
